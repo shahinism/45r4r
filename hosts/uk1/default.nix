1b9556eb9f1a18a038c8e99b9eb58c7494ef3696
Shahin Azad <hi@shahinism.com>
2024-03-02 14:52:35 +0100

    feat(uk1): enable mosh and tailscale.

--- hosts/uk1/default.nix
+++ hosts/uk1/default.nix
@@ -9,5 +9,27 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxr0CoUGmzn4nPIhddJbZzOYy1WpCkewbiSTa8BKp4c shahin"
   ];
+
+  programs = { mosh.enable = true; };
+
+  # Enable Tailscale
+  environment.systemPackages = with pkgs; [ tailscale ];
+
+  services = { tailscale = { enable = true; }; };
+
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [ 22 ];
+  };
+
   system.stateVersion = "23.11";
 }
