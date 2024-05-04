816f1f408a928856610e939ef01a7f3adca05eab
Shahin Azad <hi@shahinism.com>
2024-05-04 23:16:04 +0200

    update.

--- home/features/emacs/emacs.default/init.el
+++ home/features/emacs/emacs.default/init.el
@@ -18,6 +18,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/shahin/org/engineering.org" "/home/shahin/org/learning.org" "/home/shahin/org/meetings.org" "/home/shahin/org/para.org" "/home/shahin/org/projects.org" "/home/shahin/org/todo.org"))
  '(package-selected-packages '(blackout el-get straight hydra leaf-keywords leaf)))
 
 (custom-set-faces
