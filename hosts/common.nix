7ab204b373c1a0d1fcc3296545e76e8d2c2692ff
Shahin Azad <hi@shahinism.com>
2024-05-08 21:36:24 +0200

    feat(security): Enable basic security measures.

--- hosts/common.nix
+++ hosts/common.nix
@@ -3,7 +3,7 @@
 let syncting = { };
 in {
 
-  imports = [ ./nix.nix ./keyd.nix ./desktop.nix ];
+  imports = [ ./nix.nix ./keyd.nix ./desktop.nix ./security.nix ];
   # Kernel
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
---
+++ hosts/security.nix
@@ -0,0 +1,103 @@
+{ pkgs, ... }: {
+
+  # NOTE enabling this breaks qtile from starting!
+  # Enable more aggressive memory corruption mitiation
+  # environment.memoryAllocator.provider = "scudo";
+  # environment.variables.SCUDO_OPTIONS = "ZeroContents=1";
+
+  # Hardening the kernel
+  security.protectKernelImage = true;
+  security.forcePageTableIsolation = true;
+  # Allow building packages from source
+  security.allowUserNamespaces = true;
+  # Don't allow loading modules dynamically after boot
+  security.lockKernelModules = true;
+
+  # app armor
+  security.apparmor.enable = true;
+  security.apparmor.killUnconfinedConfinables = true;
+
+  # boot kernel parameters
+  boot.kernelParams = [
+    # Don't merge slabs
+    "slab_nomerge"
+
+    # Overwrite free'd pages
+    "page_poison=1"
+
+    # Enable page allocator randomization
+    "page_alloc.shuffle=1"
+
+    # Disable debugfs
+    "debugfs=off"
+
+    # Enable mitigations
+    "mitigations=auto,nosmt"
+
+    # Better entropy, may lead to longer boot time
+    "random.trust_cpu=off"
+    "random.trust_bootloader=off"
+  ];
+
+  boot.blacklistedKernelModules = [
+    # Obscure network protocols
+    "ax25"
+    "netrom"
+    "rose"
+
+    # Old or rare or insufficiently audited filesystems
+    "adfs"
+    "affs"
+    "bfs"
+    "befs"
+    "cramfs"
+    "efs"
+    "erofs"
+    "exofs"
+    "freevxfs"
+    "f2fs"
+    "hfs"
+    "hpfs"
+    "jfs"
+    "minix"
+    "nilfs2"
+    "ntfs"
+    "omfs"
+    "qnx4"
+    "qnx6"
+    "sysv"
+    "ufs"
+  ];
+
+  # enable firewall
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
+
+  # disable coredump that could be exploited and by nature, slows down
+  # the system, when something crashes.
+  systemd.coredump.enable = false;
+
+  # NOTE you can list firejailed session using `firejail --list`
+  programs.firejail = {
+    enable = true;
+    wrappedBinaries = {
+      # NOTE when managing firefox with home-manager, the name firefox
+      # will be overwritten. So I use a unique name, to make the
+      # wrapped binary accessible.
+      jailed-firefox = {
+        executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
+        profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+      };
+
+      tor-browser = {
+        executable = "${pkgs.tor-browser-bundle-bin}/bin/tor-browser";
+        profile = "${pkgs.firejail}/etc/firejail/tor-browser.profile";
+      };
+    };
+  };
+
+  # Enable Antivirus and keep signature DB update
+  services.clamav.daemon.enable = true;
+  services.clamav.updater.enable = true;
+}
