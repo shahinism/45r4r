526b83ff1eccbdd77c70759a4ae99ca30791dc7b
Shahin Azad <hi@shahinism.com>
2024-08-07 17:27:32 +0200

    feat(desktop): add wofi to use in hyprland.

--- home/features/desktop/rofi.nix
+++ home/features/desktop/rofi.nix
@@ -6,5 +6,8 @@
     package = pkgs.rofi.override { plugins = with pkgs; [ rofi-emoji ]; };
   };
 
-  home.file.".local/share/rofi/themes/zenburn.rasi".source = ./rofi/zenburn.rasi;
+  programs.wofi = { enable = true; };
+
+  home.file.".local/share/rofi/themes/zenburn.rasi".source =
+    ./rofi/zenburn.rasi;
 }
